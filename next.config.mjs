2f170cdf56825918fedc006ab932deda49841e32
Raisul Islam Tonmoy <sodiumrdx@gmail.com>
2026-03-03 21:22:13 +0600

    [Dashboard Added user]

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,46 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.filminquiry.com",
+      },
+      {
+        protocol: "https",
+        hostname: "shared.fastly.steamstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.curveonline.co.uk",
+      },
+      {
+        protocol: "https",
+        hostname: "gamingbolt.com",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "thumbs.dreamstime.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.engineersgarage.com",
+      },
     ],
   },
 };
